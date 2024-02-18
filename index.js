653f9aa855be4d79e17667bc364047eaf3942682
Micu-Alex <alexmicu389@gmail.com>
2024-02-19 01:28:37 +0200

    displayed pieces

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 const gameBoard = (function () {
     const board = new Array(64).fill("")
     const getBoard = () => board
-    return {getBoard}
-})()
 
-const gameContorller = (function () {
+    //puts pieces on board 
     const pieces = [
         "T", "H", "B", "Q", "K", "B", "H", "T",
          "P", "P", "P", "P", "P", "P", "P", "P"]
-
-    const board = gameBoard.getBoard()
-
+    
     const white = board.slice(0, 32)
-    const blacks = board.slice(32, 65)
-
-   for(let i = 0; i < white.length / 2; i++ ) {
+    let blacks = board.slice(32, 65)
+    
+    for(let i = 0; i < white.length / 2; i++ ) {
     white[i] = pieces[i]
-   }
-   const getWhite = () => white
-   return {getWhite}
+    }
+    blacks = white.toReversed();
+    const IndexOfK = blacks.findIndex(el => el === "K")
+    
+    blacks[IndexOfK] = "Q"
+    blacks[IndexOfK + 1] = "K"
+    
+    console.log(blacks);
+    
+    const addedPieces = [...white, ...blacks]
+    const getAddedPieces = () => addedPieces
+    return {getAddedPieces, getBoard}
+})()
+
+const gameContorller = (function () {
 
 })()
 
 const displayController = (function () {
-    const white = gameContorller.getWhite()
+    const addedPieces = gameBoard.getAddedPieces()
     const board = gameBoard.getBoard()
     const container = document.querySelector(".container")
 
     board.forEach((_, index) => {const square = document.createElement("div")
     square.classList.add("square")
     square.id = index
-    square.innerHTML = white[index]
+    square.innerHTML = addedPieces[index]
+    
     container.appendChild(square)
 })
 })()
