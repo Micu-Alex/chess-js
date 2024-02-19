fca292cd6c41a360182ad60db38fe3d1636212aa
Micu-Alex <alexmicu389@gmail.com>
2024-02-19 03:51:37 +0200

    added box index

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const gameBoard = (function () {
-    const board = new Array(64).fill("")
+    const rows = 8;
+    const columns = 8;
+
+
+    const board = new Array(rows).fill().map(() => Array(columns).fill(""))
+  
     const getBoard = () => board
 
     //puts pieces on board 
     const pieces = [
-        "T", "H", "B", "Q", "K", "B", "H", "T",
-         "P", "P", "P", "P", "P", "P", "P", "P"]
+        ["T", "H", "B", "Q", "K", "B", "H", "T"], 
+        ["P", "P", "P", "P", "P", "P", "P", "P"],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
+
     
-    const white = board.slice(0, 32)
-    let blacks = board.slice(32, 65)
-    
-    for(let i = 0; i < white.length / 2; i++ ) {
-    white[i] = pieces[i]
-    }
-    blacks = white.toReversed();
-    const IndexOfK = blacks.findIndex(el => el === "K")
-    
-    blacks[IndexOfK] = "Q"
-    blacks[IndexOfK + 1] = "K"
-    
-    console.log(blacks);
-    
+    const half = Math.ceil(board.length / 2);
+
+    const white = board.slice(0, half)
+    .map((row, index) => row = pieces[index])
+
+    const blacks = white.toReversed()
+
     const addedPieces = [...white, ...blacks]
+
+    board.forEach((_, index) => board[index] = addedPieces[index])
+
     const getAddedPieces = () => addedPieces
     return {getAddedPieces, getBoard}
 })()
 
 const gameContorller = (function () {
-
+   
 })()
 
 const displayController = (function () {
@@ -35,11 +39,29 @@ const displayController = (function () {
     const board = gameBoard.getBoard()
     const container = document.querySelector(".container")
 
-    board.forEach((_, index) => {const square = document.createElement("div")
-    square.classList.add("square")
-    square.id = index
-    square.innerHTML = addedPieces[index]
-    
-    container.appendChild(square)
-})
+    const columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+    board.forEach((row, rowIndex) => row.forEach((_, columnIndex) => {
+        const square = document.createElement("div")
+        
+        square.classList.add("square")
+        const squareIndex = columns[columnIndex] + (8 - rowIndex);
+        square.id = squareIndex
+        
+        const piece = document.createElement("div")
+        piece.classList.add("piece")
+        piece.innerHTML = addedPieces[rowIndex][columnIndex]
+        square.appendChild(piece)
+
+        square.addEventListener("click", (e) => console.log(e.target.id))
+        
+        const p = document.createElement("p")
+        p.classList.add("marking")
+        p.innerHTML = square.id
+        square.appendChild(p)
+
+        container.appendChild(square)
+    })
+    ) 
 })()
+
