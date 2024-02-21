04b6b8ca539c4882c8912f513dc0f61c6888b56f
Micu-Alex <alexmicu389@gmail.com>
2024-02-21 02:50:13 +0200

    rwerwebq

--- index.js
+++ index.js
@@ -1,67 +1,70 @@
-const gameBoard = (function () {
-    const rows = 8;
-    const columns = 8;
+const  Pieces  = {
+    WHITE_PAWN: '♙',
+    WHITE_ROOK: '♖',
+    WHITE_KNIGHT: '♘',
+    WHITE_BISHOP: '♗',
+    WHITE_QUEEN: '♕',
+    WHITE_KING: '♔',
+
+    BLACK_PAWN: '♟',
+    BLACK_ROOK: '♜',
+    BLACK_KNIGHT: '♞',
+    BLACK_BISHOP: '♝',
+    BLACK_QUEEN : '♛',
+    BLACK_KING: '♚'
+}
+
+const board = [
+    [Pieces.BLACK_ROOK, Pieces.BLACK_KNIGHT, Pieces.BLACK_BISHOP, Pieces.BLACK_QUEEN, Pieces.BLACK_KING, Pieces.BLACK_BISHOP, Pieces.BLACK_KNIGHT, Pieces.BLACK_ROOK], 
+    [Pieces.BLACK_PAWN,Pieces.BLACK_PAWN, Pieces.BLACK_PAWN, Pieces.BLACK_PAWN, Pieces.BLACK_PAWN, Pieces.BLACK_PAWN,Pieces. BLACK_PAWN, Pieces.BLACK_PAWN],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [Pieces.WHITE_PAWN,Pieces.WHITE_PAWN, Pieces.WHITE_PAWN, Pieces.WHITE_PAWN, Pieces.WHITE_PAWN, Pieces.WHITE_PAWN,Pieces. WHITE_PAWN, Pieces.WHITE_PAWN],
+    [Pieces.WHITE_ROOK, Pieces.WHITE_KNIGHT, Pieces.WHITE_BISHOP, Pieces.WHITE_QUEEN, Pieces.WHITE_KING, Pieces.WHITE_BISHOP, Pieces.WHITE_KNIGHT, Pieces.WHITE_ROOK], 
+]
 
 
-    const board = new Array(rows).fill().map(() => Array(columns).fill(""))
-  
-    const getBoard = () => board
 
-    //puts pieces on board 
-    const pieces = [
-        ["T", "H", "B", "Q", "K", "B", "H", "T"], 
-        ["P", "P", "P", "P", "P", "P", "P", "P"],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '']]
+const getBoardState = function() {
+    const pieces = document.querySelectorAll(".piece");
+    let updatedBoard = [];
+    pieces.forEach(piece => {board.push(piece.innerText)});
 
-    
-    const half = Math.ceil(board.length / 2);
 
-    const white = board.slice(0, half)
-    .map((row, index) => row = pieces[index])
+    console.log(updatedBoard);
+    return updatedBoard
+}
 
-    const blacks = white.toReversed()
+const displayController = (function() {
 
-    const addedPieces = [...white, ...blacks]
-
-    board.forEach((_, index) => board[index] = addedPieces[index])
-
-    const getAddedPieces = () => addedPieces
-    return {getAddedPieces, getBoard}
-})()
-
-const gameContorller = (function () {
-   
-})()
-
-const displayController = (function () {
-    const addedPieces = gameBoard.getAddedPieces()
-    const board = gameBoard.getBoard()
     const container = document.querySelector(".container")
-
     const columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+
     let dragged;
-    board.forEach((row, rowIndex) => row.forEach((_, columnIndex) => {
+
+    board.forEach((row, rowIndex) => row.forEach((_, columnIndex)=> {
+
         const square = document.createElement("div")
         
         square.classList.add("square")
         const squareIndex = columns[columnIndex] + (8 - rowIndex);
         square.id = squareIndex
         
-        if ( addedPieces[rowIndex][columnIndex] !== "") {
-            const piece = document.createElement("div")
-            piece.id = squareIndex   
+            const piece = document.createElement("div")  
             piece.classList.add("piece")
             piece.draggable = true
-            piece.innerHTML = addedPieces[rowIndex][columnIndex]
+            piece.innerHTML = board[rowIndex][columnIndex]
             square.appendChild(piece)
        
             piece.addEventListener("dragstart", (e) => {
                 dragged = e.target
+
             })
         
-        }
-       
+    
         square.addEventListener("dragover", (e) => {
             e.preventDefault();
             false
@@ -69,7 +72,9 @@ const displayController = (function () {
 
         square.addEventListener("drop", (e) => {
             e.preventDefault();
+            
             e.target.appendChild(dragged)
+            board = getBoardState()
         })
         
         const marking = document.createElement("p")
@@ -78,7 +83,5 @@ const displayController = (function () {
         square.appendChild(marking)
 
         container.appendChild(square)
-    })
-    ) 
+    } ))
 })()
-
