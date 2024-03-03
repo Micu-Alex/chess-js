78596707b69bfb5b0dd28f17fdd61aec054a4c42
Micu-Alex <alexmicu389@gmail.com>
2024-03-03 18:27:08 +0200

    added comments

--- scripts/displayController.js
+++ scripts/displayController.js
@@ -3,8 +3,9 @@ import gameControl from "./gameControl.js";
 
 const displayController = function() {
     const container = document.querySelector(".container")
-       let dragged;
+    let dragged;
     
+    // Function to handle the drop event
     function handleDrop(event, square) {
         gameControl(dragged, square, event);
         event.preventDefault();
@@ -12,50 +13,57 @@ const displayController = function() {
         console.log(board);
     }
     
-    
+    // Looping through each row and column to create squares
     board.forEach((row, rowIndex) => row.forEach((_, columnIndex)=> {
 
         const square = document.createElement("div");
         
         square.classList.add("square");
+
+        // Generating a unique ID for each square based on its position
         const squareIndex = columns[columnIndex] + rows[rowIndex];
         square.id = squareIndex;
-        //add color to square
+
+        // Adding color to the square based on its position
         if ((rowIndex + columnIndex) % 2 === 0) {
             square.classList.add("black-square");
         } else {
             square.classList.add("white-square");
         }
         
+        // Checking if there is a piece in the current board position
         if (board[rowIndex][columnIndex] !== "") {
-
+            // Creating a piece element
             const piece = document.createElement("div")  ;
             piece.classList.add("piece");
             piece.draggable = true;
             piece.innerHTML = board[rowIndex][columnIndex];
             square.appendChild(piece);
-        
+            // Adding a dragstart event listener to the piece
             piece.addEventListener("dragstart", (event) => {
                 dragged = event.target;
     
             });
         };
         
-    
+        // Adding dragover event listener to the square
         square.addEventListener("dragover", (event) => {
+            // Preventing the default dragover behavior
             event.preventDefault();
-            false;
         });
 
+        // Adding drop event listener to the square
         square.addEventListener("drop", (event) => {
             handleDrop(event, square);
         });
         
+        // Creating a marking element to display the square's ID
         const marking = document.createElement("p");
         marking.classList.add("marking");
         marking.innerHTML = square.id;
         square.appendChild(marking);
 
+        // Appending the square to the container
         container.appendChild(square);
     }));
 
